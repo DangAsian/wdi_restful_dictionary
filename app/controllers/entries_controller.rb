039afcc054a5828718d072e5a39596b4c1c0356e
Daniel Ang <dan.justin.ang@gmail.com>
2018-09-27 17:06:15 -0400

    [restful_dictionary 27-Sep-2018] added the self, new, and create method and respective functions

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,7 +1,6 @@
 class EntriesController < ApplicationController
   def index
     @entries = Entry.all
-
   end
 
   def show
@@ -9,15 +8,22 @@ class EntriesController < ApplicationController
   end
 
   def new
-
+    @entry = Entry.new
   end
 
   def create
+    @entry = Entry.new
+    @entry.word = params[:entry][:word]
+    @entry.definition = params[:entry][:definition]
+    @entry.language = params[:entry][:language]
+
+    @entry.save
+
     redirect_to entries_url
   end
 
   def edit
-    render :edit
+    @entry = Entry.find(params[:id])
   end
 
   def update
